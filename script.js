adca69246d613a08bf1a2fc63c2b808811fab1d1
Wu Wei <vc3104@gmail.com>
2024-03-26 00:02:27 +0900

    version 2

--- script.js
+++ script.js
@@ -1,53 +1,104 @@
-// const envelope = document.getElementById('envelope');
-
-// window.addEventListener('scroll', () => {
-//     const scrollY = window.scrollY;
-//     const envelopeTop = envelope.getBoundingClientRect().top;
-
-//     if (scrollY > envelopeTop - window.innerHeight / 2) {
-//         envelope.classList.add('opened');
-//     }
-// });
-
 window.addEventListener("scroll", setScrollVar)
 window.addEventListener("resize", setScrollVar)
-
+window.onload = function () {
+    setScrollVar()
+}
 function setScrollVar() {
     const htmlElement = document.documentElement
     const percentOfScreenHeightScrolled =
         htmlElement.scrollTop / htmlElement.clientHeight
-    var sectionHeightScrolled = Math.min(percentOfScreenHeightScrolled * 100, 100)
+    var sectionHeightScrolled = percentOfScreenHeightScrolled * 100
     htmlElement.style.setProperty(
         "--scroll",
         sectionHeightScrolled
     )
 
     openEnvelop(sectionHeightScrolled)
+    setCardAnimation(sectionHeightScrolled)
 }
 
 function openEnvelop(sectionHeightScrolled) {
+    if (sectionHeightScrolled >= 63) {
+        document.querySelector("#hearts").classList.add("open")
+        document.querySelector("#hearts").classList.remove("close")
+    } else {
+        document.querySelector("#hearts").classList.add("close")
+        document.querySelector("#hearts").classList.remove("open")
+    }
+
     if (sectionHeightScrolled >= 66) {
         document.querySelector("#front-flap").classList.add("opened")
-    }
-    else {
+        document.querySelector("#hearts").classList.add("open")
+        document.querySelector("#hearts").classList.remove("close")
+    } else {
         document.querySelector("#front-flap").classList.remove("opened")
     }
 }
 
-const observer = new IntersectionObserver(entries => {
-    for (let i = entries.length - 1; i >= 0; i--) {
-        const entry = entries[i]
-        if (entry.isIntersecting) {
-            document.querySelectorAll("[data-img]").forEach(img => {
-                img.classList.remove("show")
-            })
-            const img = document.querySelector(entry.target.dataset.imgToShow)
-            img?.classList.add("show")
-            break
+function setCardAnimation(sectionHeightScrolled) {
+    if (sectionHeightScrolled >= 141.3) {
+        document.querySelector("#card1").classList.add("takeout-to-center")
+        if (sectionHeightScrolled >= 155) {
+            document.querySelector("#card1").classList.add("takeout-to-below")
+        } else {
+            document.querySelector("#card1").classList.remove("takeout-to-below")
         }
+    } else {
+        document.querySelector("#card1").classList.remove("takeout-to-center")
     }
+
+    if (sectionHeightScrolled >= 220) {
+        document.querySelector("#card2").classList.add("takeout-to-center")
+        if (sectionHeightScrolled >= 234) {
+            document.querySelector("#card2").classList.add("takeout-to-below")
+        } else {
+            document.querySelector("#card2").classList.remove("takeout-to-below")
+        }
+    } else {
+        document.querySelector("#card2").classList.remove("takeout-to-center")
+    }
+
+    if (sectionHeightScrolled >= 304) {
+        document.querySelector("#card3").classList.add("takeout-to-center")
+        if (sectionHeightScrolled >= 314) {
+            document.querySelector("#card3").classList.add("takeout-to-below")
+        } else {
+            document.querySelector("#card3").classList.remove("takeout-to-below")
+        }
+    } else {
+        document.querySelector("#card3").classList.remove("takeout-to-center")
+    }
+}
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            var scrollDiv = document.getElementById("third").offsetTop;
+            window.scrollTo({ top: scrollDiv, behavior: 'smooth' });
+        } else {
+        }
+    })
 })
 
-document.querySelectorAll("[data-img-to-show]").forEach(section => {
-    observer.observe(section)
-})
+observer.observe(document.querySelector("#third"))
+
+var countDownDate = new Date("July 6, 2024 00:00:00").getTime();
+
+var countdownTimer = setInterval(function () {
+    var now = new Date().getTime();
+
+    var distance = countDownDate - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("countdown").innerHTML = days + "d " + hours + "h "
+        + minutes + "m " + seconds + "s ";
+
+    if (distance < 0) {
+        clearInterval(countdownTimer);
+        document.getElementById("countdown").innerHTML = "EXPIRED";
+    }
+}, 1000);
